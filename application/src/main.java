70ce4970475b933a8dfebefdef0e23cfa60df10e
Qalipso <31683331+Qalipso@users.noreply.github.com>
2019-06-27 22:23:46 +0300

    start
    input

---
+++ application/src/main.java
@@ -0,0 +1,10 @@
+import mypack.InputConsole;
+
+public class main {
+    public static void main(String[] args) {
+        InputConsole data = new InputConsole();
+        //data.get_patterns();
+        System.out.println(data.forChange);
+        System.out.println(data.onChange);
+    }
+}
